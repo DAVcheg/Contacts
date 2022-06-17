2517857da4684a9121c35204de4e2fb9acc9f384
Aleksey Dergunov <davcheg@me.com>
2022-06-17 16:58:13 +1000

    Table configuration using reusable cells

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -17,3 +17,27 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard var cell = tableView.dequeueReusableCell(withIdentifier: "MyCell") else {
+            print ("Создаем новую ячейку для строки с индексом \(indexPath.row)")
+            var newCell = UITableViewCell(style: .default, reuseIdentifier: "MyCell")
+            configure(cell: &newCell, for: indexPath)
+            return newCell
+        }
+        print ("Используем старую ячейку для строки с индексом \(indexPath.row)")
+        configure(cell: &cell, for: indexPath)
+        return cell
+    }
+    private func configure(cell: inout UITableViewCell, for indexPath: IndexPath) {
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = "Строка \(indexPath.row)"
+        cell.contentConfiguration = configuration
+    }
+    
+}
+
